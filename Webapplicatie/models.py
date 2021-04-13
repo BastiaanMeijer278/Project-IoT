3948e0cc3707480c7508b8ed154e4ac2c2c76862
BastiaanMeijer <b.meijer@st.hanze.nl>
2021-04-13 13:21:19 +0200

    Databasekoppeling en refactoring

--- Webapplicatie/models.py
+++ Webapplicatie/models.py
@@ -2,17 +2,22 @@ from flask_sqlalchemy import *
 from Webapplicatie import db
 
 class Verblijf(db.Model):
+    __tablename__ = 'Verblijf'
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.Text)
-
+    dieren = db.relationship('Dier', backref='Verblijf')
     def __init__(self, name):
         self.name = name
 
 class Dier(db.Model):
+    __tablename__ = 'Dier'
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.Text)
     detected = db.Column(db.Boolean)
-    verblijf = db.Column(db.Integer)
+    verblijf = db.Column(db.Integer, db.ForeignKey('Verblijf.id'))
+
+    
+
     
 class Sensor(db.Model):
     id = db.Column(db.Integer, primary_key = True)
