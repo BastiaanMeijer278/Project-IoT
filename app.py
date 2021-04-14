bd53e29727ae5efdd0e9ca36b3ab876059fd2d74
BastiaanMeijer <b.meijer@st.hanze.nl>
2021-04-14 12:48:32 +0200

    Databasekoppeling met Webapp.

--- app.py
+++ app.py
@@ -5,7 +5,25 @@ from flask import render_template
 @app.route('/')
 def index():
     residences = Verblijf.query.all()
-    return render_template('home.html', residence=residences)
+    animals = Dier.query.all()
+    
+    d = {}
+    for i in residences:
+        d[i.id] = []
+        soort = Diersoort.query.filter_by(verblijf=i.id).all()
+        for j in soort:
+            d[i.id] += [k for k in animals if k.soort == j.id and k.detected == True]
+            if Dier.query.filter_by(soort=j.id, detected=False).all():
+                alarm = True
+            else:
+                alarm = False
 
-if __name__ =='__main__':
+    soorten = {}
+    for i in Diersoort.query.all():
+        soorten[i.id] = ''
+        soorten[i.id] += i.name
+
+    return render_template('home.html', residence=residences, list_of_animals=d, soorten=soorten, alarm=alarm)
+
+if __name__ == '__main__':
     app.run(debug=True)
--- setup_database.py
+++ setup_database.py
@@ -1,13 +1,50 @@
 from Webapplicatie import db, Verblijf
 from Webapplicatie.models import *
+import random
+import names
 
 db.create_all()
 
-savannenhuis = Verblijf('Savannenhuis')
 serenga = Verblijf('Serenga')
 jungola = Verblijf('Jungola')
 nortica = Verblijf('Nortica')
-animazia = Verblijf('Amimazia')
+animazia = Verblijf('Animazia')
 
-db.session.add_all([savannenhuis, serenga, jungola, nortica, animazia])
+db.session.add_all([serenga, jungola, nortica, animazia])
+ijsberen = Diersoort('Ijsbeer', 'Nortica')
+db.session.add_all([ijsberen])
+
+Henk = Dier('Ijsbeer', 'Henk', True)
+db.session.add_all([Henk])
+
 db.session.commit()
+
+witte_neushoorn = Diersoort('Witte neushoorn', 'Serenga')
+nijlpaard = Diersoort('Nijlpaard', 'Serenga')
+ringstaartmaki = Diersoort('Ringstaartmaki', 'Jungola')
+zuidelijke_pelsrob = Diersoort('Zuidelijke pelsrob', 'Nortica')
+bonte_vari = Diersoort('Bonte vari', 'Jungola')
+groene_zeeschildpad = Diersoort('Groene zeeschildpad', 'Animazia')
+sporenschildpad = Diersoort('Sporenschildpad', 'Animazia')
+
+db.session.add_all([witte_neushoorn, nijlpaard, ringstaartmaki, zuidelijke_pelsrob, bonte_vari, groene_zeeschildpad, sporenschildpad])
+
+Dieren = []
+for i in Diersoort.query.all():
+    for j in range(random.randint(3,5)):
+        Dieren += [Dier(i.name, names.get_first_name(), True)] 
+
+
+db.session.add_all(Dieren)
+
+pieter = Dier('Nijlpaard', 'Pieter', False)
+db.session.add_all([pieter])
+
+db.session.commit()
+
+
+
+
+
+# db.session.add_all([ijsberen])
+# db.session.commit
