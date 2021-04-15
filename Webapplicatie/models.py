87e724f4075bc16af6334ac8e2bb6ff2b02ea289
LethalNL1 <78829086+LethalNL1@users.noreply.github.com>
2021-04-15 12:37:43 +0200

    Add files via upload

--- Webapplicatie/models.py
+++ Webapplicatie/models.py
@@ -1,4 +1,14 @@
-from Webapplicatie import db
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+db = SQLAlchemy(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'd42cee127ae26605af73cc0365302fc5'
 
 class Verblijf(db.Model):
 
@@ -18,7 +28,16 @@ class Diersoort(db.Model):
 
     def __init__(self, name, verblijf):
         self.name = name
-        self.verblijf = Verblijf.query.filter_by(name=verblijf).first().id
+        self.verblijf = verblijf
+
+class Device(db.Model):
+
+    __tablename__ = "Device"
+    id = db.Column(db.Integer, primary_key = True)
+    address = db.Column(db.Text)
+
+    def __init__(self, address):
+        self.address = address
     
 class Dier(db.Model):
 
@@ -26,11 +45,13 @@ class Dier(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     soort = db.Column(db.Integer, db.ForeignKey("Diersoort.id"))
     name = db.Column(db.Text)
+    device = db.Column(db.Integer, db.ForeignKey("Device.id"))
     detected = db.Column(db.Boolean)
 
-    def __init__(self, soort, name, detected):
-        self.soort = Diersoort.query.filter_by(name=soort).first().id
+    def __init__(self, soort, name, device, detected):
+        self.soort = soort
         self.name = name
+        self.device = device
         self.detected = detected
 
 class Sensor(db.Model):
@@ -47,10 +68,11 @@ class Data(db.Model):
     __tablename__ = "Data"
     id = db.Column(db.Integer, primary_key = True)
     sensor = db.Column(db.Integer, db.ForeignKey("Sensor.id"))
-    dier = db.Column(db.Text,db.ForeignKey("Dier.id"))
+    dier = db.Column(db.Integer,db.ForeignKey("Dier.id"))
     output = db.Column(db.Text)
 
     def __init__(self, sensor, dier, output):
         self.sensor = sensor
         self.dier = dier
         self.output = output
+        
